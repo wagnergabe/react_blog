05bc476804be48ccedefcadff472f5ecbb37dafd
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2022-10-26 19:37:30 -0500

    added basic components

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,16 +1,15 @@
 
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = "Welcome to the new blog";
-  const likes = 50;
 
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{ title }</h1>
-        <p> Liked { likes } times</p>
-        <p>{Math.random() * 2}</p>
+       <Home />
       </div>
     </div>
   );
