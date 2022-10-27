a1e6b8b40681096eca4dfc3a01ca3e8f36c1fa51
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2022-10-26 21:38:20 -0500

    add basic click events

--- blog/src/Home.js
+++ blog/src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log('hello')
+    }
+
+    const handleClickAgain = (name) => {
+        console.log(`hello ${name}`)
+    }
+
     return ( 
         <div className="home">
         <h2>Homepage</h2>
+        <button onClick={handleClick}>click here</button>
+        <button onClick={() => handleClickAgain('Gabe')}>Click me again</button>
         </div>
      );
 }
